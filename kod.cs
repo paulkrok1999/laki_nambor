d7ad132230746cf9d002c751718daf2e0811116c
paulkrok1999 <paulshilo1999@gmail.com>
2026-03-10 18:49:37 +0000

    1.0.3

--- kod.cs
+++ kod.cs
@@ -1,62 +1,55 @@
-/*
-создание угадай число 
-я хочу чтобы пользватель выбирал уровень
-легко от 0 до 20
-средний от 0 до 50 
-сложный от 0 до 100 и тд.
-еще нужно что бы были поддсказки больше и меньши
-*/
-class TestClass
-{
-    static void Main()
-    {
-        Console.Write("пр что бы начать играть выбери уровень" +
-            "\n 1. легко от 0 до 20" +
-            "\n 2. средний от 0 до 50 "+
-            "\n 3. сложный от 0 до 100 и тд."
+class TestClass {
+    static void PlayGame() {
+        Console.WriteLine("Выберите уровень сложности: ");
+        Console.WriteLine("1 - Легкий(1-20, 10 попыток)"
+            + "\n 2 - Средний(1-50, 7 попыток)"
+            + "\n 3 - Сложный(1-50, 5 попыток)"
         );
-        int num = 1;
-        switch (num)
-        {
-            case 1:
-                Random random = new();
-                int rand = random.Next(1, 20);
-                int po = 0;
-                for (int i = 0; i < rand; i++)
-                {
-                    Console.Write("введи чило от 0 до 10: ");
-                    string message = Console.ReadLine();
-                    string res = $"{rand}";
-                    if (res == message) { Console.WriteLine($"вы угадали число : {rand}"); }
-                    else { Console.WriteLine($"вы не угадали число"); }
-                }
+        Console.Write("Ваш выбор: ");
+        int num;//макс число
+        int pop;//макс попытки
+        string pt = Console.ReadLine();//выбор сложности        
+        switch (pt) {
+            case "1":
+                num = 20; pop = 10;
                 break;
-            case 2:
-                Random random1 = new();
-                int rand1 = random1.Next(1, 50);
-                int po1 = 0;
-                for (int i = 0; i < rand1; i++)
-                {
-                    Console.Write("введи чило от 0 до 10: ");
-                    string message = Console.ReadLine();
-                    string res = $"{rand1}";
-                    if (res == message) { Console.WriteLine($"вы угадали число : {rand1}"); }
-                    else { Console.WriteLine($"вы не угадали число"); }
-                }
+            case "2":
+                num = 50; pop = 7;
                 break;
-            case 3:
-                Random random2 = new();
-                int rand2 = random2.Next(1, 100);
-                int po2 = 0;
-                for (int i = 0; i < rand2; i++)
-                {
-                    Console.Write("введи чило от 0 до 10: ");
-                    string message = Console.ReadLine();
-                    string res = $"{rand2}";
-                    if (res == message) { Console.WriteLine($"вы угадали число : {rand2}"); }
-                    else { Console.WriteLine($"вы не угадали число"); }
-                }
+            case "3":
+                num = 100; pop = 5;
+                break;
+            default:
+                Console.WriteLine("Вы ничего не выбрали вы начинаете с среднего уровня");
+                num = 50; pop = 7;
                 break;
         }
-    } 
+        Random random = new();
+        int sekret = random.Next(1, num + 1);
+        Console.WriteLine($"я загадал число от 1 до {num}. у вас {pop} попыток.");
+        int attempts = 0;
+        bool guessed = false;
+        while (attempts < num && !guessed) {
+            Console.Write($"попытка {attempts + 1}/{num}. Введите число: ");
+            if (!int.TryParse(Console.ReadLine(), out int guess)) {
+                Console.WriteLine("ошипка введите целое число"); continue;
+            }
+            attempts++;//слолько было сделано попыток
+            if (guess < sekret) { Console.WriteLine("больше"); }
+            else if (guess > sekret) { Console.WriteLine("меньше"); }
+            else {
+                guessed = true;
+                Console.WriteLine($"поздравляю число {sekret} за {pop} попыток!");
+            }
+            // после того как угадал число
+            if (attempts == 1) { Console.WriteLine("c 1 попытки!"); }
+            else if (attempts <= num / 2) Console.WriteLine($"отличный результат!-{attempts}");
+            else { Console.WriteLine($"тоже норм-{attempts}"); }
+        }
+        //если не угадал число и истратил
+        if (!guessed) Console.WriteLine($"попытки кончились я загадал {sekret}");
+    }
+    static void Main() {
+        PlayGame();
+    }
 }
